2baef93d659dda85ad4aea16c82fbf67518f01ab
Amritha <amrithagopal3027@gmail.com>
2026-04-19 11:14:14 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,13 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    PROJECT_NAME: str = "FastAPI Task Manager"
-    # Do NOT hardcode secrets! Loading from environment variable.
+    PROJECT_NAME: str = "FastAPI Task Manager" 
     SECRET_KEY: str = os.getenv("SECRET_KEY", "fallback-secret-for-local")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
-    
-    # If running on Vercel, the file system is read-only except for /tmp
+    #while deploying on vercel i have to be careful of env 
     if os.getenv("VERCEL"):
         DATABASE_URL: str = "sqlite:////tmp/taskmanager.db"
     else:
